3dc75e3040e1bc47f9864f4203134308abef48cf
AmmanRizwan <ammanrizwan00007@gmail.com>
2025-01-28 11:28:07 +0530

    popup window set

--- Client/src/components/PopUp.jsx
+++ Client/src/components/PopUp.jsx
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom';
 const PopUp = ({serial, iconSrc, bgSrc, name, year, mature, category, type, description, OnClose}) => {
   return (
     <div id={serial} className={`flex justify-center items-center min-h-screen fixed inset-0 z-50 bg-black bg-opacity-50 backdrop-blur-lg transition-all duration-600 shadow-xl z-[1000]`}>
-      <div className="w-[670px] border border-1 border-gray-500 rounded-xl relative">
+      <div className="mx-4 w-[670px] border border-1 border-gray-500 rounded-xl relative">
         <div>
           <img className="w-[670px] rounded-t-xl" src={bgSrc} alt={name} />
         </div>
 
-        <div className="w-[250px] h-[150px] absolute bottom-[250px] left-10">
-          <img className="object-contain w-[250px] h-[150px]" src={iconSrc} alt={"Default Icon"} />
+        <div className="sm:w-[250px] sm:h-[150px] w-[150px] h-[150px] absolute bottom-[150px] left-5 sm:bottom-[250px] sm:left-10">
+          <img className=" object-contain sm:w-[250px] sm:h-[150px]" src={iconSrc} alt={"Default Icon"} />
         </div>
 
         <div onClick={OnClose} className="absolute top-3 rounded-full right-3 hover:bg-red-50 transition-all cursor-pointer duration-500">
@@ -24,17 +24,17 @@ const PopUp = ({serial, iconSrc, bgSrc, name, year, mature, category, type, desc
           <div className="bg-bgBlack rounded-b-xl flex flex-col sm:p-10 sm:pt-4 p-4">
             
 
-              <ul className="flex gap-4">
-                <li>{year}</li>
-                <li>{mature}</li>
-                <li>{type}</li>
-                <li>{category}</li>
+              <ul className="list-disc px-2 text-sm sm:text-base flex gap-4">
+                <li className="mx-2">{year}</li>
+                <li  className="mx-2">{mature}</li>
+                <li  className="mx-2">{type}</li>
+                <li  className="mx-2">{category}</li>
               </ul>
-            <div className="pt-4 text-lg h-[100px]">{description}</div>
+            <div className="pt-4 text-sm sm:text-lg h-[100px]">{description}</div>
           
           <div className="pt-4">
-          <Link to={'/signup'}><Button title="Get Started" classBtn={'px-6 py-2 bg-red-600 font-semibold text-xl hover:bg-red-700 transition-all duration-500 gap-2'} RightIcon={<GoChevronRight size={32} />} /></Link>
-          <p>Create or restart your membership</p>
+          <Link to={'/signup'}><button className="btn rounded-full bg-red-600 hover:bg-red-700 text-white mb-3">Get Started</button></Link>
+          <p className="text-sm sm:text-lg">Create or restart your membership</p>
           </div>
       </div>
       
